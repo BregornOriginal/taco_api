274ed85dbd0ac18499773200a5b84e68d4475ec7
Julio <juliog_14@hotmail.com>
2023-06-01 00:06:39 -0300

    Create tacos controller with crud actions for angular app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/app', to: 'angular_app#index'
 
+  resources :tacos, only: [:index, :new, :create, :show, :destroy]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
